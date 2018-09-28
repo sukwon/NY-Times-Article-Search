4651079b6ad7e2ddda9e320178a0a342e69b024d
Sukwon Lee <slee@hearsaycorp.com>
2018-09-27 22:31:57 -0700

    Add filter activity

---
+++ app/src/main/java/com/codepath/newyorktimesarticlesearch/activities/FilterActivity.java
@@ -0,0 +1,36 @@
+package com.codepath.newyorktimesarticlesearch.activities;
+
+import android.app.DatePickerDialog;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.DatePicker;
+
+import com.codepath.newyorktimesarticlesearch.R;
+import com.codepath.newyorktimesarticlesearch.fragments.DatePickerFragment;
+
+import java.util.Calendar;
+
+public class FilterActivity extends AppCompatActivity implements DatePickerDialog.OnDateSetListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_filter);
+    }
+
+    // DataPicker
+
+    public void showDatePickerDialog(View v) {
+        DatePickerFragment newFragment = new DatePickerFragment();
+        newFragment.show(getSupportFragmentManager(), "datePicker");
+    }
+
+    @Override
+    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+        final Calendar c = Calendar.getInstance();
+        c.set(Calendar.YEAR, year);
+        c.set(Calendar.MONTH, month);
+        c.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+    }
+}
